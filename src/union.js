88c0c326fe6ade5d8b742e2768ae38f8b776f735
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-08-20 15:21:05 +0600

    behuda js removed

--- src/union.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const newDev = {
-    name: 'John',
-    expertise: 'Frontend',
-    experience: 1
-};
-// so, union type is a type that can be one of several types
-// in this case, newDev can be either noobDev or juniorDev
